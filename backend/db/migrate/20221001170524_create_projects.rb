c3f8dead913eb06fcf2f2f60486c5615e35767ef
NickeaBennett <nickeabennett@gmail.com>
2022-10-04 11:04:20 -0400

    Update project database attribute to string

--- backend/db/migrate/20221001170524_create_projects.rb
+++ backend/db/migrate/20221001170524_create_projects.rb
@@ -2,16 +2,16 @@ class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
       t.string :project_name
-      t.text :project_description
+      t.string :project_description
       t.string :slug
       t.string :image_url
       t.string :project_website_url
       t.string :project_discord_url
       t.string :project_twitter_url
       t.string :project_opensea_url
-      t.integer :project_max_supply
-      t.decimal :project_unit_price_eth
-      t.datetime :project_sale_date
+      t.string :project_max_supply
+      t.string :project_unit_price_eth
+      t.string :project_sale_date
       t.string :minting_contract_address
       t.string :project_blockchain
       t.boolean :is_verified
